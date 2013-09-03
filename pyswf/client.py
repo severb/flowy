49d0096e8236ea5b8e240d9f2d19090784a8084e
Sever Băneşiu <banesiu.sever@gmail.com>
2013-09-03 14:53:32 +0300

    New layout.

--- pyswf/client.py
+++ pyswf/client.py
@@ -1,36 +1,66 @@
 import json
 from boto.swf.layer1 import Layer1
 
-class SWFClient(object):
-    def __init__(self, workflows=[], activities=[], client=None):
-        self.workflows = dict((w.id, w) for w in workflows)
-        self.activities = dict((a.id, a) for a in activities)
+
+# job_interface:
+# id
+# __call__(context)
+
+# scheduled_job
+# id
+# args
+# kwargs
+# token
+
+class BaseClient(object):
+    def __init__(self, jobs, client=None):
+        self.job_runners = {}
+        for job in jobs:
+            self.register_job_runner(job)
         self.client = client is not None and client or Layer1()
 
-    def run(self, domain, task_list):
+    def register_job_runner(self, job):
+        self.job_runners[job.id] = job
+
+    def select_job_runner(self, job_id):
+        return self.job_runners[job_id]
+
+    def run(self, domain, task_lits):
         while 1:
-            if self.workflows:
-                self.advance_next_workflow(domain, task_list)
-            if self.activities:
-                self.run_next_activity(domain, task_list)
+            self.process_next_job(domain, task_list)
 
-    def advance_next_workflow(self, domain, task_list):
-        response = self.poll_next_decision(domain, task_list)
-        wf_history = WorkflowHistory(response)
-        Workflow = self.select_workflow(wf_history.id)
-        workflow_runner = Workflow(wf_history)
-        args = self.input_to_args(wf_history.input)
-        kwargs = self.input_to_kwargs(wf_history.input)
-        scheduled = workflow_runner(*args, **kwargs)
-        self.send_decision_results(wf_history, scheduled)
+    def process_next_job(self, domain, task_list):
+        context, scheduled_job = self.poll_next_job(domain, task_list)
+        Runner = self.select_job_runner(scheduled_job.id)
+        job_runner = Runner(context)
+        job_result = job_runner(*scheduled_job.args, **scheduled_job.kwargs)
+        self.save_result(scheduled_job.token, job_result)
 
-    def poll_next_decision(self, domain, task_list):
-        return self.client.poll_for_decision_task(domain, task_list)
+    def poll_next_job(self, domain, task_list):
+        raise NotImplemented()
 
-    def select_workflow(self, workflow_id):
-        return self.workflows[workflow_id]
+    def save_result(self, job_result):
+        raise NotImplemented()
 
-    def send_decision_results(self, wf_history, scheduled):
+
+class ScheduledWorkflow(object):
+    def __init__(self, response):
+        self.id = ('a', '1')
+        self.args = []
+        self.kwargs = {}
+        self.token = '123'
+
+
+class WorkflowClient(BaseClient):
+    def register_job_runner(self, workflow):
+        super(self, WorkflowClient).register_job_runner(workflow)
+        # XXX: check if the workflow is registered
+
+    def poll_next_job(self, domain, task_list):
+        response = self.client.poll_for_decision_task(domain, task_list)
+        return WorkflowHistory(response), ScheduledWorkflow(response)
+
+    def save_result(self, workflow_token, scheduled_tasks):
         l = Layer1Decisions()
         for invocation_id, activity, args, kwargs in scheduled:
             l.schedule_activity_task(
@@ -44,41 +74,45 @@ class SWFClient(object):
             decisions=l._data
         )
 
-    def run_next_activity(self, domain, task_list):
-        response = self.poll_next_activity(domain, task_list)
-        activity_spec = ActivitySpecification(response)
-        activity_runner = self.select_activity(actvitiy_spec.id)
-        args = self.input_to_args(activity_spec.input)
-        kwagrs = self.input_to_kwargs(activity_spec.input)
-        result = activity(*args, **kwargs)
-        self.send_activity_result(activity_spec, result)
 
-    def poll_next_activity(self, domain, task_list):
-        return self.client.poll_for_activity_task(domain, task_list)
+class ScheduledActivity(object):
+    def __init__(self, response):
+        self.id = ('a', '1')
+        self.args = []
+        self.kwargs = {}
+        self.token = '123'
 
-    def select_activity(self, activity_spec_id):
-        return self.activities[activity_spec_id]
 
-    def send_activity_result(self, activity_spec, result):
-        self.client.respond_activity_task_completed(
-            activity_spec.task_token,
-            self.serialize_result(result)
-        )
+class ActivityClient(BaseClient):
+    def register_job_runner(self, activity):
+        super(self, WorkflowClient).register_job_runner(workflow)
+        # XXX: check if the activity is registered
 
-    def input_to_args(self, input):
-        return json.loads(input)['args']
+    def poll_next_job(self, domain, task_list):
+        response = self.client.poll_for_activity_task(domain, task_list)
+        return None, ScheduledActivity(response)
 
-    def input_to_kwargs(self, input):
-        return json.loads(input)['kwargs']
+    def save_result(self, activity_token, activity_result):
+        self.client.respond_activity_task_completed(activity_token, activity_result)
 
-    def serialize_input(self, *args, **kwargs):
-        return json.dumps({
-            'args': args,
-            'kwargs': kwargs
-        })
 
-    def serialize_result(self, result):
-        return json.dumps({
-            'error': False,
-            'context': result
-        })
+class SWFClient(object):
+    def __init__(self, workflows=[], activities=[], client=None):
+        self.workflow_client = WorkflowClient(workflows, client)
+        self.activity_client = WorkflowClient(activities, client)
+
+    def run(self):
+        while 1:
+            self.workflow_client.process_next_job()
+            self.activity_client.process_next_job()
+
+    def register_activity_runner(self, activity):
+        self.activity_client.register_job_runner(activity)
+
+    def register_workflow_runner(self, workflow):
+        self.workflow_client.register_job_runner(workflow)
+
+
+class ThreadedSWFClient(SWFClient):
+    def run(self):
+        pass
