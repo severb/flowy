ba9b9a246fcce13ca6f7d6de511bfb2ebed30e9b
Sever Băneşiu <banesiu.sever@gmail.com>
2013-09-05 15:34:57 +0300

    Added new example.

--- examples/prime_workflow_parallel.py
+++ examples/prime_workflow_parallel.py
@@ -2,20 +2,23 @@ from pyswf.workflow import Workflow, ActivityProxy
 from pyswf.client import WorkflowClient
 
 
-class PrimeTest(Workflow):
-    name = 'PrimeTestWorkflow2'
+class PrimeTestParallel(Workflow):
+    name = 'PrimeTestWorkflow3'
     version = 1
 
     div = ActivityProxy('Divider2', 1)
 
     def run(self, n=None):
         n = n if n is not None else 7 * 11
+        scheduled = []
         for i in range(2, n/2 + 1):
-            if self.div(n, i).result():
+            scheduled.append(self.div(n, i))
+        for s in scheduled:
+            if s.result():
                 print '%s is divisible by %s' % (n, i)
                 break
 
 
 
-c = WorkflowClient('SeversTest', 'prime_task_list', [PrimeTest])
+c = WorkflowClient('SeversTest', 'prime_task_list', [PrimeTestParallel])
 c.run()
