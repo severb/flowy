6beca244b267be4359ebe0fdaebc5050aa17ef78
Sever Băneşiu <banesiu.sever@gmail.com>
2013-11-06 11:26:28 +0200

    Removed old examples.

--- examples/prime_scheduler.py
+++
@@ -1,9 +0,0 @@
-from flowy.client import workflow_client
-
-prime_w = workflow_client.scheduler_on(
-    domain='SeversTest',
-    name='MyPrime',
-    version=2,
-    task_list='prime_task_list',
-)
-prime_w()
