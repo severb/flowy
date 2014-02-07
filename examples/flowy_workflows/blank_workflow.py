87f6e928eac028e155169a055d221af0442e9d42
Roland Szabo <rolisz@yahoo.com>
2014-02-07 15:17:01 +0200

    Start fixing integration tests

--- examples/flowy_workflows/blank_workflow.py
+++ examples/flowy_workflows/blank_workflow.py
@@ -1,33 +1,31 @@
-from flowy import Workflow, ActivityProxy, WorkflowProxy
-from flowy import make_config, workflow_config
+from flowy.swf.boilerplate import start_workflow_worker
+from flowy.swf.scanner import workflow
+from flowy.task import Workflow
+from flowy.swf.starter import WorkflowStarter
 
 
-@workflow_config('BlankWorkflow', 3, 'a_list', task_start_to_close=60,
-                 execution_start_to_close=60)
+@workflow('BlankWorkflow', 3, 'a_list', decision_duration=60,
+          workflow_duration=60)
 class BlankWorkflow(Workflow):
     """
     Does nothing
 
     """
 
-    def run(self, remote):
+    def run(self):
         return True
 
 
 if __name__ == '__main__':
-    my_config = make_config('RolisTest')
-
     f = open("/home/local/3PILLAR/rszabo/flowy/mocks_output.txt", "w")
     f.close()
     f = open("/home/local/3PILLAR/rszabo/flowy/mocks.txt", "w")
     f.close()
 
     # Start a workflow
-    BlankWorkflowId = my_config.workflow_starter('BlankWorkflow', 3, task_list='b_list')
-    print 'Starting: ', BlankWorkflowId()
+    BlankWorkflowId = WorkflowStarter('RolisTest', 'BlankWorkflow', 3,
+                                      task_list='b_list')
+    BlankWorkflowId()
+    print('started')
 
-    from time import sleep
-    sleep(0)
-    # Start the workflow loop
-    my_config.scan()
-    my_config.start_workflow_loop(task_list='b_list')
+    start_workflow_worker('RolisTest', task_list='b_list', loop=5)
