038e89df2fa42aa7ade291491c4deb88f42a8afb
Sever Băneşiu <banesiu.sever@gmail.com>
2014-06-19 16:54:58 +0300

    Added starter

--- flowy/boilerplate.py
+++ flowy/boilerplate.py
@@ -1,13 +1,14 @@
 import sys
+import uuid
 
 from boto.swf.layer1 import Layer1
 from flowy import logger, MagicBind
+from flowy.poller import SWFActivityPoller, SWFWorkflowPoller
+from flowy.proxy import serialize_args
 from flowy.scanner import SWFScanner
-from flowy.poller import SWFWorkflowPoller, SWFActivityPoller
-from flowy.worker import SingleThreadedWorker
+from flowy.spec import _sentinel, SWFWorkflowSpec
 from flowy.task import AsyncSWFActivity
-from flowy.spec import SWFWorkflowSpec
-from flowy.starter import SWFWorkflowStarter
+from flowy.worker import SingleThreadedWorker
 
 
 def start_activity_worker(domain, task_list,
@@ -76,3 +77,35 @@ def _get_client(layer1, domain):
     if layer1 is None:
         layer1 = Layer1()
     return MagicBind(layer1, domain=str(domain))
+
+
+class SWFWorkflowStarter(object):
+    def __init__(self, spec, client, id=None, tags=None):
+        self._spec = spec
+        self._client = client
+        self._id = id
+        self._tags = tags
+
+    def options(self, task_list=_sentinel, decision_duration=_sentinel,
+                workflow_duration=_sentinel, id=_sentinel, tags=_sentinel):
+        old_tags = tags
+        old_id = id
+        if id is not _sentinel:
+            self._id = id
+        if tags is not _sentinel:
+            self._tags = tags
+
+        with self._spec.options(task_list, decision_duration,
+                                workflow_duration):
+            yield
+        self._id = old_id
+        self._tags = old_tags
+
+    def start(self, *args, **kwargs):
+        id = self._id
+        if id is None:
+            id = uuid.uuid4()
+        input = self._serialize_arguments(*args, **kwargs)
+        return self._spec.start(self._client, id, input, self._tags)
+
+    _serialize_arguments = serialize_args
