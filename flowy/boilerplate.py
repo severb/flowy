26478358b47522fe4d8b0382d50e9c3963cf75fe
Sever Băneşiu <banesiu.sever@gmail.com>
2014-06-19 16:32:42 +0300

    Starter

--- flowy/boilerplate.py
+++ flowy/boilerplate.py
@@ -3,9 +3,11 @@ import sys
 from boto.swf.layer1 import Layer1
 from flowy import logger, MagicBind
 from flowy.scanner import SWFScanner
-from flowy.poller import SWFWorkflowPoller
+from flowy.poller import SWFWorkflowPoller, SWFActivityPoller
 from flowy.worker import SingleThreadedWorker
-from flowy.task import SWFWorkflow, SWFScheduler
+from flowy.task import AsyncSWFActivity
+from flowy.spec import SWFWorkflowSpec
+from flowy.starter import SWFWorkflowStarter
 
 
 def start_activity_worker(domain, task_list,
@@ -15,19 +17,18 @@ def start_activity_worker(domain, task_list,
                           package=None,
                           ignore=None):
     swf_client = _get_client(layer1, domain)
-    Spec = TaskSpec
-    if reg_remote:
-        Spec = RemoteActivitySpec
-    scanner = Scanner(ActivitySpecCollector(Spec, swf_client))
+    scanner = SWFScanner()
     scanner.scan_activities(package=package, ignore=ignore, level=1)
-    poller = ActivityPoller(swf_client, task_list)
+    poller = SWFActivityPoller(swf_client, task_list, scanner)
     worker = SingleThreadedWorker(poller)
     not_registered = scanner.register(worker)
-    if not_registered:
-        logger.error(
-            'Not all activities could be regestered: %r', not_registered
-        )
-        sys.exit(1)
+    if reg_remote:
+        not_registered = scanner.register_remote(swf_client)
+        if not_registered:
+            logger.error(
+                'Not all activities could be regestered: %r', not_registered
+            )
+            sys.exit(1)
     try:
         worker.run_forever(loop)
     except KeyboardInterrupt:
@@ -58,23 +59,17 @@ def start_workflow_worker(domain, task_list,
         pass
 
 
-# def async_scheduler(domain, layer1=None):
-#     return AsyncActivityScheduler(client=_get_client(layer1, domain))
+def async_scheduler(domain, token, layer1=None):
+    return AsyncSWFActivity(_get_client(layer1, domain), token)
 
 
-# def workflow_starter(domain, name, version,
-#                      task_list=None,
-#                      decision_duration=None,
-#                      workflow_duration=None,
-#                      layer1=None):
-#     return WorkflowStarter(
-#         name=name,
-#         version=version,
-#         client=_get_client(layer1, domain),
-#         task_list=task_list,
-#         decision_duration=decision_duration,
-#         workflow_duration=workflow_duration
-#     )
+def workflow_starter(domain, name, version, task_list=None,
+                     decision_duration=None, workflow_duration=None,
+                     id=None, tags=None, layer1=None):
+    spec = SWFWorkflowSpec(name, version, task_list, decision_duration,
+                           workflow_duration)
+    client = _get_client(layer1, domain)
+    return SWFWorkflowStarter(spec, client, id, tags)
 
 
 def _get_client(layer1, domain):
