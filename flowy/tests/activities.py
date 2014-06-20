42b3b6f36830cf18562e2bcc350fcfd61bd02fc9
Sever Băneşiu <banesiu.sever@gmail.com>
2014-06-20 12:12:17 +0300

    Update activities.

--- flowy/tests/activities.py
+++ flowy/tests/activities.py
@@ -1,45 +1,45 @@
 import time
 
-from flowy.swf.scanner import activity
-from flowy.task import Activity
+from flowy.scanner import swf_activity as activity
+from flowy.task import SWFActivity as Activity
 
 
-@activity('Identity', 77, 'example_list')
+@activity(77, 'example_list')
 class Identity(Activity):
 
     def run(self, n):
         return n
 
 
-@activity('Double', 77, 'example_list')
+@activity(77, 'example_list')
 class Double(Activity):
 
     def run(self, n):
         return n * 2
 
 
-@activity('Sum', 77, 'example_list')
+@activity(77, 'example_list')
 class Sum(Activity):
 
     def run(self, *n):
         return sum(n)
 
 
-@activity('Square', 77, 'example_list')
+@activity(77, 'example_list')
 class Square(Activity):
 
     def run(self, n):
         return n ** 2
 
 
-@activity('Error', 77, 'example_list')
+@activity(77, 'example_list')
 class Error(Activity):
 
     def run(self, msg='err'):
         raise RuntimeError(msg)
 
 
-@activity('Timeout', 77, 'example_list', start_to_close=2)
+@activity(77, 'example_list', start_to_close=2)
 class Timeout(Activity):
 
     def run(self):
