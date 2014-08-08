14f165e9e1a0998f942f91fa3fb7537700b849d0
Cristina Botez <cristina.botez@3pillarglobal.com>
2014-08-08 10:18:21 +0300

    Added fix for tag list encoder. Fixed test with tag lists. Bumped version to 0.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flowy',
-    version='0.3.0',
+    version='0.3.1',
     description="Python library for Amazon Simple Workflow Service",
     long_description=open('README.rst').read(),
     keywords='AWS SWF workflow',
